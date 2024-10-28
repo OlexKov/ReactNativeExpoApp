3e89e9f6f9137ecf7585be731c9c26ae459d17ef
OlexKov <Kovalchuk.olex@gmail.com>
2024-10-28 13:06:05 +0200

    Update

--- constants/Url.ts
+++ constants/Url.ts
@@ -1,6 +1,6 @@
-//export const BASE_URL: string = "http://3.72.67.233:5088";
-export const BASE_URL: string = "http://10.0.2.2:5147";
-export const BASE_API_URL: string = "http://10.0.2.2:5147/api";
+export const BASE_URL: string = "http://3.72.67.233:5088";
+//export const BASE_URL: string = "http://10.0.2.2:5147";
+export const BASE_API_URL: string = BASE_URL + "/api";
 export const CATEGORY_API_URL: string = BASE_API_URL + "/Category"
 export const IMAGE_FOLDER:string = "/images"
 export const IMAGE_200_URL:string = BASE_URL + IMAGE_FOLDER + "/200_"
